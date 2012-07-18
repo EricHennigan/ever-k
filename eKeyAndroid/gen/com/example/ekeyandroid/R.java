f476a5b0b507cd4ad8a14841527286a0dca856e0
freewillywonka <whillhou@uci.edu>
2012-07-17 18:18:56 -0700

    one key

--- eKeyAndroid/gen/com/example/ekeyandroid/R.java
+++ eKeyAndroid/gen/com/example/ekeyandroid/R.java
@@ -20,7 +20,8 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int button1=0x7f080000;
+        public static final int menu_settings=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -30,7 +31,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int button=0x7f050004;
         public static final int hello_world=0x7f050001;
+        public static final int k=0x7f050005;
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_main=0x7f050003;
     }
