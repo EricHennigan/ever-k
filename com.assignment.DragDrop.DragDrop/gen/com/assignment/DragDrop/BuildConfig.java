1244d7c0981818765028ce5f69dbfc45d34614a2
Anh Cao <dark_knight_kingdom@yahoo.com>
2012-08-13 19:11:34 -0700

    Modify menu

---
+++ com.assignment.DragDrop.DragDrop/gen/com/assignment/DragDrop/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.assignment.DragDrop;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- com.assignment.DragDrop.DragDrop/src/com/assignment/DragDrop/DragDrop.java
+++ com.assignment.DragDrop.DragDrop/src/com/assignment/DragDrop/DragDrop.java
@@ -1,11 +1,12 @@
 package com.assignment.DragDrop;
 
+
+
 //import com.assignment.ekey2.DrawView;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-//import android.view.MenuItem;
-//import android.view.View.OnDragListener;
+import android.view.MenuItem;
 
 public class  DragDrop extends Activity {
     /** Called when the activity is first created. */
@@ -21,7 +22,11 @@ public class  DragDrop extends Activity {
     //jl added below from original eKeyAndroid project. also added menu\activity_main.xml and view.menu import
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+    	super.onCreateOptionsMenu(menu);
+    	
+    	MenuItem item = menu.add("Add");
+    	item = menu.add("Edit");
+    	getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
     
